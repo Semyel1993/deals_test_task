04d1736f91fc3791d0ab43a3eeb4ff13a8f0b21a
Semyel1993 <sementip@gmail.com>
2023-10-03 01:34:31 +0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" python manage.py createsuperuser --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --noinput
+DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" python manage.py createsuperuser --username "$DJANGO_SUPERUSER_USERNAME" --noinput --email "$DJANGO_SUPERUSER_EMAIL"
 python manage.py runserver 0.0.0.0:8000
